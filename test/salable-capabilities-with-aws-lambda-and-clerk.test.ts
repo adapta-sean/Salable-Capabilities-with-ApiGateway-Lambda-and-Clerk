74d0db9accda9c1b298e5b7a7aab9d3dc3373322
Sean Cooper <scooper@adaptavist.com>
2024-06-05 20:34:38 +0100

    Initial commit

---
+++ test/salable-capabilities-with-aws-lambda-and-clerk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SalableCapabilitiesWithAwsLambdaAndClerk from '../lib/salable-capabilities-with-aws-lambda-and-clerk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/salable-capabilities-with-aws-lambda-and-clerk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SalableCapabilitiesWithAwsLambdaAndClerk.SalableCapabilitiesWithAwsLambdaAndClerkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
